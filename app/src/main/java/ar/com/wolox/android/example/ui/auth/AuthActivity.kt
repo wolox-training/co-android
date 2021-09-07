98b6512f1b72c524a519a47e17de7aef90e1227b
Camilo Ortiz <juan.c.ortiz.arango@accenture.com>
2021-09-07 13:21:09 -0500

    add  WoloxAndroidBootstrap

---
+++ app/src/main/java/ar/com/wolox/android/example/ui/auth/AuthActivity.kt
@@ -0,0 +1,15 @@
+package ar.com.wolox.android.example.ui.auth
+
+import ar.com.wolox.android.R
+import ar.com.wolox.android.databinding.ActivityBaseBinding
+import ar.com.wolox.android.example.ui.auth.login.LoginFragment
+import ar.com.wolox.wolmo.core.activity.WolmoActivity
+import javax.inject.Inject
+
+class AuthActivity @Inject constructor() : WolmoActivity<ActivityBaseBinding>() {
+    override fun layout() = R.layout.activity_base
+
+    override fun init() {
+        replaceFragment(binding.activityBaseContent.id, LoginFragment.newInstance())
+    }
+}
